039f783e606144eeafc33d790ba26bcb201aebec
BobbyKJH <rlatnguscjsw@gmail.com>
2022-04-20 13:45:53 +0900

    feat: nav logo, background-color

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -5,7 +5,11 @@ function Nav() {
   return (
     <div className={styles.body}>
       <Link to="/" className={styles.title}>
-        StarBucks
+        <img
+          className={styles.logo}
+          src="https://t1.daumcdn.net/cfile/tistory/99857F4F5E738F472F"
+          alt="starbucks logo"
+        />
       </Link>
       <Link to="/menu" className={styles.content}>
         2
