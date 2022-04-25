ff1975ff6f947a501b6e7dfb6a2dc8350074278c
BobbyKJH <rlatnguscjsw@gmail.com>
2022-04-25 13:36:09 +0900

    README.md and Create BeverageMenu

--- src/components/BeverageMenu.jsx
+++ src/components/BeverageMenu.jsx
@@ -1,16 +1,28 @@
 import { useState } from "react";
 import styles from "./BeverageMenu.module.css";
+import SecondNav from "./SecondNav";
 
 function BeverageMenu() {
+  const [coffee, setCoffee] = useState(true);
+
+  const btn = () => {
+    setCoffee(!coffee);
+  };
   return (
     <div className={styles.body}>
       <div className={styles.menu}>
-        <button className={styles.select}>1</button>
+        <button className={styles.select} onClick={btn}>
+          1
+        </button>
         <button className={styles.select}>2</button>
         <button className={styles.select}>3</button>
         <button className={styles.select}>4</button>
       </div>
+
+      {coffee ? <SecondNav /> : <div></div>}
     </div>
   );
 }
 export default BeverageMenu;
+
+// 삼항 연산자를 사용하여 메뉴를 불러오는 방식으로 제작한다.
