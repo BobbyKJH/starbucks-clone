365d50c3cd8e68eb1596c5139f8706cf586bcc04
BobbyKJH <rlatnguscjsw@gmail.com>
2022-04-26 13:50:19 +0900

    Json file create

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,12 +4,12 @@ import styles from "./Home.module.css";
 
 function Home() {
   return (
-    <>
+    <div className={styles.backGround}>
       {/* 메인 nav */}
       <Nav />
       {/* 서브 nav */}
-      <div className={styles.SecondNav}></div>
-    </>
+      <div className={styles.SecondNav}>1111</div>
+    </div>
   );
 }
 export default Home;
--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -5,13 +5,13 @@ import styles from "./SignIn.module.css";
 
 function SignIn() {
   return (
-    <>
+    <div className={styles.backGround}>
       <Nav />
       <div className={styles.SecondNav}>
         <SecondNav name={"HOME"} menu={"SignIn"} />
       </div>
       <SignInBox />
-    </>
+    </div>
   );
 }
 export default SignIn;
